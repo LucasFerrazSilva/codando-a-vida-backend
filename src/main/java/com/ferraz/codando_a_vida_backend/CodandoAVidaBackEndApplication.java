71abd47d60f5be6df1dbed123f5f4c6e7f4ad353
Lucas Ferraz <lucasferrazsilva@hotmail.com>
2024-09-17 08:59:49 -0300

    add project

---
+++ src/main/java/com/ferraz/codando_a_vida_backend/CodandoAVidaBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.ferraz.codando_a_vida_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodandoAVidaBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodandoAVidaBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ferraz/codando_a_vida_backend/CodandoAVidaBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ferraz.codando_a_vida_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodandoAVidaBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
